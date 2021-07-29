acae63b7bb1d601f80e2d7dc16cdfb4f1f1335ac
SaLiK ImtiaZ <r.salikimtiaz@gmail.com>
2021-07-29 21:59:47 +0500

    code refactored in case study pag

--- pages/work/[id].jsx
+++ pages/work/[id].jsx
@@ -34,7 +34,7 @@ function CaseStudy() {
             <h1 className="xs:text-30 md:text-36 lg:text-44 xl:text-48 text-primary leading-tight font-semibold mt-4 -mb-0">
               Toronto Fine Jewelry.ca
             </h1>
-            <p className="xs:text-base md:text-2xl font-light text-gray md:max-w-45 xs:my-6 md:my-6 border-l-4 pl-4">
+            <p className="xs:text-20 md:text-24 font-light text-gray md:max-w-55 xs:my-6 md:my-6 border-l-4 pl-4 leading-relaxed">
               Rebranding and generating exponential user growth poses
               challenges. That's why Neuvoo, one of the world's largest job
               sites, chose Orizon to seamlessly transition their brand to
@@ -98,8 +98,10 @@ function CaseStudy() {
       <section>
         <div className="lg:container lg:mx-auto pt-28">
           <h4 className="text-12 font-semibold text-gray">VISUAL IDENTITY</h4>
-          <h2 className="text-52 font-semibold">Logotype: A single image</h2>
-          <p className="text-28 font-light mt-4 max-w-45 mb-10">
+          <h2 className="xs:text-32 lg:text-52 font-semibold">
+            Logotype: A single image
+          </h2>
+          <p className="xs:text-16 lg:text-22 font-light mt-4 max-w-65 mb-4 leading-loose">
             The arrow-shaped logo was inspired by the previous one that
             represented a rocket (which is the international symbol of growth!).
             With that detail, we’ve managed to slim the logo, while ensuring
